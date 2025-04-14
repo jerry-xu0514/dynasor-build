90ba008c1ea05c90c225336849143d1b3a9f13e8
Jerry Xu <jerriven@gmail.com>
2025-04-14 15:34:38 -0700

    added aws configure to pull and run script

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -14,6 +14,11 @@ CONTAINER_NAME="trtllm_container"
 ################################################################################
 # 1) Log in to ECR
 ################################################################################
+echo ">>> Configuring AWS CLI with provided credentials"
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set default.region "$AWS_REGION"
+
 echo "Authenticating to ECR..."
 aws ecr get-login-password --region "$AWS_REGION" \
   | docker login --username AWS --password-stdin "$ECR_REPO_URI"
